fd4dbf32f2879cac4c8a75ef141acccd3b3c969b
Mark Szymczyk <mark@meandmark.com>
2017-07-18 20:45:33 -0400

    Initial Commit

---
+++ RichTextEditor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RichTextEditor
+//
+//  Created by mark on 7/18/17.
+//  Copyright © 2017 Me and Mark Publishing. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RichTextEditor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RichTextEditor
+//
+//  Created by mark on 7/18/17.
+//  Copyright © 2017 Me and Mark Publishing. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
