5a0fda7e2da1a06638f6fa5beeba2d3e43aa79b3
Mark Szymczyk <mark@meandmark.com>
2017-07-31 20:01:24 -0400

    Add code to save the text view contents to disk when someone chooses to save the document.
    This code could be cleaner, especially removing the implicitly unwrapped conditionals. But it's a start, and the code does save the file.

--- RichTextEditor/Document.swift
+++ RichTextEditor/Document.swift
@@ -33,6 +33,12 @@ class Document: NSDocument {
     override func data(ofType typeName: String) throws -> Data {
         // Insert code here to write your document to data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning nil.
         // You can also choose to override fileWrapperOfType:error:, writeToURL:ofType:error:, or writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
+        
+        // Save the text view contents to disk
+        if let textView = viewController?.textView {
+            let textRange = NSRange(location: 0, length: (textView.string?.characters.count)!)
+            return textView.rtfd(from: textRange)!
+        }
         throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
     }
 
