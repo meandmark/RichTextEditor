c072848e55cad6454397a97e8753c37a6bea6ee8
Mark Szymczyk <mark@meandmark.com>
2017-08-01 15:23:11 -0400

    Override the viewDidAppear method to fill the text view with the document's contents.
    I would like to replace the as! clause with something safer, but I have document saving and loading working right now so I'm committing.

--- RichTextEditor/ViewController.swift
+++ RichTextEditor/ViewController.swift
@@ -15,8 +15,15 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
     }
 
+    override func viewDidAppear() {
+        // Fill the text view with the document's contents.
+        let document = self.view.window?.windowController?.document as! Document
+        textView.textStorage?.setAttributedString(document.text)
+    }
+    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
